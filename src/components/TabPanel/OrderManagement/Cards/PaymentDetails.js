7bdafe1de698b21b735f8c360c6c42b30d369ee6
Digant Kusulia <Digant.kusulia@hotmail.com>
2021-07-15 17:24:21 +0530

    Update UI-2

--- src/components/TabPanel/OrderManagement/Cards/PaymentDetails.js
+++ src/components/TabPanel/OrderManagement/Cards/PaymentDetails.js
@@ -56,33 +56,38 @@ const PaymentDetails = (props) => {
     const payment = props.paymentDetails;
 
     return (
-    <div style={{border:'1px solid #DEDBDB'}}>
+    <div style={{border:'1px solid #DEDBDB', marginRight:'20px'}}>
 
     { payment &&  
     
       <table>
-              <tr rowSpan="3" style={{backgroundColor:'#DEDBDB'}}> <th></th> <td rowSpan="1" style={{ minWidth:'260px'}}><strong style={{fontSize:'26px', opacity:'0.7'}}>Payment Details</strong> </td>    <th></th></tr>
-              <tr>
-                  <th><h6>Payment</h6></th>  <td><Typography  variant="body2" style={{color:'grey'}} component="h6">{payment.pm}</Typography></td> <td></td>
+              <tr rowSpan="3" style={{backgroundColor:'#DEDBDB'}}> <th></th> <td rowSpan="1" style={{ minWidth:'365px'}}><strong style={{fontSize:'26px', opacity:'0.7'}}>Payment Details</strong> </td>    <th></th></tr>
+              <tr >
+                  <th><h6 style={{marginLeft:'15px'}}>Payment</h6></th>  <td><Typography  variant="body2" style={{color:'grey'}} component="h6">{payment.pm}</Typography></td> <td></td>
               </tr>
 
               <tr>
-                  <th><h6>Promo Code</h6></th>  <td><Typography  variant="body2" style={{color:'grey'}} component="h6">{!payment.promo || payment.promo === null || payment.promo === '' ? 'None is Applied' : payment.promo}</Typography></td><td></td>
+                  <th><h6  style={{marginLeft:'15px'}}>Promo Code</h6></th>  <td><Typography  variant="body2" style={{color:'grey'}} component="h6">{!payment.promo || payment.promo === null || payment.promo === '' ? 'None is Applied' : payment.promo}</Typography></td><td></td>
               </tr>
 
               <tr>
-                <th style={{minWidth:'240px'}}><h6>Total Before Discount</h6></th>   <td><Typography  variant="body2" style={{color:'grey'}} component="h6">{payment.totalBeforeDiscount}.00</Typography></td><td style={{minWidth:'130px'}}></td>
-              </tr>
-              <tr>
-                <th><h6>Total Tax</h6></th>   <td><Typography  variant="body2" style={{color:'grey'}} component="h6">{payment.totalTax}.00</Typography></td><td></td>
+                <th style={{minWidth:'240px'}}><h6 style={{marginLeft:'15px'}}>Total Before Discount</h6></th>   <td><Typography  variant="body2" style={{color:'grey'}} component="h6">{payment.totalBeforeDiscount}.00</Typography></td><td style={{minWidth:'130px'}}></td>
               </tr>
 
               <tr>
-                <th><h6>Delivery Fee</h6></th>   <td><Typography  variant="body2" style={{color:'grey'}} component="h6">{payment.delivery}.00</Typography></td><td></td>
+                <th style={{minWidth:'240px'}}><h6 style={{marginLeft:'15px'}}>Discount</h6></th>   <td><Typography  variant="body2" style={{color:'green'}} component="h6">{-payment.discount}.00</Typography></td><td style={{minWidth:'130px'}}></td>
               </tr>
 
               <tr>
-                <th><h6>Total</h6></th>  <td><Typography  variant="body2"  style={{color:'grey'}} component="h6">{payment.total}.00</Typography></td><td></td>
+                <th><h6 style={{marginLeft:'15px'}}>Total Tax</h6></th>   <td><Typography  variant="body2" style={{color:'grey'}} component="h6">{payment.totalTax}.00</Typography></td><td></td>
+              </tr>
+
+              <tr>
+                <th><h6 style={{marginLeft:'15px'}}>Delivery Fee</h6></th>   <td><Typography  variant="body2" style={{color:'grey'}} component="h6">{payment.delivery}.00</Typography></td><td></td>
+              </tr>
+
+              <tr>
+                <th><h6 style={{marginLeft:'15px'}}>Total</h6></th>  <td><Typography  variant="body2"  style={{color:'grey'}} component="h6">{payment.total}.00</Typography></td><td></td>
               </tr>
           </table>
     
